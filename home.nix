14eca798566512d6723dec4c74b4b3a404d14286
josephwest <joeywest1414@gmail.com>
2024-10-09 12:48:48 -0600

    updated config

--- home.nix
+++ home.nix
@@ -32,7 +32,26 @@
 	    number = true;
 	    relativenumber = true;
 	    shiftwidth = 4;
+	    scrolloff = 8;
 	};
+	keymaps = [
+	    {
+		action = "<C-d>zz";
+		key = "<C-d>";
+	    }
+	    {
+		action = "<C-u>zz";
+		key = "<C-u>";
+	    }
+	    {
+		action = ":NvimTreeFocus <CR>";
+		key = "<Leader>t";
+	    }
+	    {
+		action = ":NvimTreeClose <CR>";
+		key = "<Leader>T";
+	    }
+	];
 	colorschemes.onedark.enable = true;
 	plugins = {
 	    telescope = {
@@ -40,6 +59,7 @@
 		keymaps = {
 		    "<leader>ff" = "find_files";
 		    "<leader>gf" = "git_files";
+		    "<leader>fg" = "live_grep";
 		};
 	    };
 	    treesitter.enable = true;
@@ -63,6 +83,23 @@
 	    };
 	    lsp-format.enable = true;
 	    cmp-nvim-lsp-signature-help.enable = true;
+	    auto-save.enable = true;
+	    nvim-tree = {
+		enable = true;
+		disableNetrw = true;
+		openOnSetup = true;
+	    };
+	    autoclose = {
+		enable = true;
+		keys =   {
+		    "(" = { escape = false; close = true; pair = "()"; };
+		    "[" = { escape = false; close = true; pair = "[]"; };
+		    "{" = { escape = false; close = true; pair = "{}"; };
+
+		    "'" = { escape = false; close = false; pair = "''"; };
+		};
+		options.autoIndent = true;
+	    };
 	};
     };
 }
