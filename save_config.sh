98a6e9ab3573454e25cc107f44657674a0f3c132
josephwest <joeywest1414@gmail.com>
2024-10-12 12:58:14 -0600

    updated config

--- save_config.sh
+++ save_config.sh
@@ -1,8 +1,7 @@
-chroot ~/.dotfiles/nixosgit
 cp /etc/nixos/* ~/.dotfiles/nixosgit
 echo saved config
 git add .
 git commit -m "updated config"
 git push -u origin main
 echo commited and pushed to github
-ls ~/.dotfiles/nixosgit | grep -xv 'save_config.sh' | xargs rm
+find ~/.dotfiles/nixosgit | grep -xv 'save_config.sh' | echo
