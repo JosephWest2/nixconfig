8c5a4690715cbdd163c7bb89f2fb8133a3d13cd0
josephwest <joeywest1414@gmail.com>
2024-10-12 13:05:09 -0600

    updated config

--- save_config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cp /etc/nixos/* ~/.dotfiles/nixosgit
-git add .
-git commit -m "updated config"
-git push -u origin main
-find -maxdepth 1 ~/.dotfiles/nixosgit -type f ! -name "save_config.sh" -delete
